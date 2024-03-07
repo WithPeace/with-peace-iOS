6cd70d528652ccfa9bd113d56e68ff6bebc8988b
dylan-yoon <d.dylany21@gmail.com>
2024-03-07 12:30:31 +0900

    feat: ProjectSetting, Stroyboard 제거

---
+++ WithPeace/WithPeace/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  WithPeace
+//
+//  Created by Dylan_Y on 3/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
