86409ceb3807cc342ad0686cc69a6f94a1828d44
hemg2 <hemg2@naver.com>
2024-03-08 14:56:12 +0900

    feat: KeychainManager생성

--- WithPeace/WithPeace/ViewController.swift
+++ WithPeace/WithPeace/ViewController.swift
@@ -10,6 +10,8 @@ import GoogleSignIn
 
 final class ViewController: UIViewController {
     
+    private let keychainManager = KeychainManager()
+    
     //TODO: - 이미지 변경
     let signInButton: GIDSignInButton = {
         let btn = GIDSignInButton()
@@ -34,27 +36,67 @@ final class ViewController: UIViewController {
         NSLayoutConstraint.activate([
             signInButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
         ])
+        
+        retrieveTokens()
     }
     
     @objc
     func tabGoogle() {
         GIDSignIn.sharedInstance.signIn(withPresenting: self) { signInResult, error in
-          guard error == nil else { return }
+            guard error == nil else { return }
             //TODO: 로그인 성공적으로 받아지면 실행되는 main View 담아주는 곳.
-          // If sign in succeeded, display the app's main content View.
+            // If sign in succeeded, display the app's main content View.
             
             guard let signInResult = signInResult else { return }
             
             let user = signInResult.user
-            let emailAddress = user.profile?.email
-            let fullName = user.profile?.name
-            let givenName = user.profile?.givenName
-            let familyName = user.profile?.familyName
-            let profilePicUrl = user.profile?.imageURL(withDimension: 320)
-            
             let idtoken = user.idToken?.tokenString
             let accesstoken = user.accessToken.tokenString
             let refreshtoken = user.refreshToken.tokenString
+            
+            if let accessTokenData = accesstoken.data(using: .utf8) {
+                do {
+                    try self.keychainManager.save(account: "accessToken", password: accessTokenData)
+                    print("되는거야? 엑세스")
+                } catch {
+                    print("accessToken Kechain error: \(error.localizedDescription)")
+                }
+            }
+            
+            if let refreshTokenData = refreshtoken.data(using: .utf8) {
+                do {
+                    try self.keychainManager.save(account: "refreshToken", password: refreshTokenData)
+                    print("되는거야? 리프레쉬")
+                } catch {
+                    print("Keychain error: \(error.localizedDescription)")
+                }
+            }
         }
+    }
+}
+
+//MARK: 키체인로직
+extension ViewController {
+    
+    func retrieveTokens() {
+        if let accessTokenData = keychainManager.get(account: "accessToken"),
+           let accessToken = String(data: accessTokenData, encoding: .utf8) {
+            print("access token: \(accessToken)")
+        } else {
+            print("Access token 없음.")
+        }
+        
+        if let refreshTokenData = keychainManager.get(account: "refreshToken"),
+           let refreshToken = String(data: refreshTokenData, encoding: .utf8) {
+            print("refresh token: \(refreshToken)")
+        } else {
+            print("Refresh token 없음.")
+        }
+    }
+    
+    //TODO: 토큰삭제할때 사용하기
+    private func logoutUser() {
+        keychainManager.delete(account: "accessToken")
+        keychainManager.delete(account: "refreshToken")
     }
 }
