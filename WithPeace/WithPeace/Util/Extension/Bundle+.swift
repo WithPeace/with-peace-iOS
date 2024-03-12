c1433d60a0f2c547b234cc19faa419761be0edf9
dylan-yoon <d.dylany21@gmail.com>
2024-03-12 13:17:44 +0900

    chore: 기본 폴더링

---
+++ WithPeace/WithPeace/Util/Extension/Bundle+.swift
@@ -0,0 +1,19 @@
+//
+//  Bundle+.swift
+//  WithPeace
+//
+//  Created by Dylan_Y on 3/12/24.
+//
+
+import Foundation
+
+extension Bundle {
+    var apiKey: String? {
+            guard let file = self.path(forResource: "Secrets", ofType: "plist"),
+                  let resource = NSDictionary(contentsOfFile: file),
+                  let key = resource["API_KEY"] as? String else {
+                return nil
+            }
+            return key
+        }
+}
