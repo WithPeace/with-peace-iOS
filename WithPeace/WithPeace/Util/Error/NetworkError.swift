cfd396ff9cbe057ef953cb1694aa4f31b66acb27
Dylan-Yoon <77507952+Dylan-yoon@users.noreply.github.com>
2024-03-12 14:07:38 +0900

    Feat/#1 network setup (#4)
    * feat: network manager 구현
    
    * refactor: return 처리

---
+++ WithPeace/WithPeace/Util/Error/NetworkError.swift
@@ -0,0 +1,19 @@
+//
+//  NetworkError.swift
+//  WithPeace
+//
+//  Created by Dylan_Y on 3/12/24.
+//
+
+enum NetworkError: Error {
+    case defaultsError
+    case responseError
+    case getDataError
+    
+    case badRequest
+    case unauthorized
+    case forbidden
+    case notFound
+    case unprocessableEntity
+    case internalServerError
+}
