40e8c48109e464e6765508d49f90b38d4be2a314
hemg2 <hemg2@naver.com>
2024-03-21 14:18:16 +0900

    refactor: Description text키보드액션 추가

--- WithPeace/WithPeace/View/Post/Cell/DescriptionCell.swift
+++ WithPeace/WithPeace/View/Post/Cell/DescriptionCell.swift
@@ -8,13 +8,22 @@
 import UIKit
 import RxSwift
 
-final class DescriptionCell: UITableViewCell {
-    private let descriptionTextField: UITextField = {
-        let textField = UITextField()
-        textField.translatesAutoresizingMaskIntoConstraints = false
-        textField.placeholder = "내용을 입력해주세요"
+final class DescriptionCell: UITableViewCell, UITextViewDelegate {
+    private lazy var descriptionTextView: UITextView = {
+        let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.delegate = self
+        textView.font = UIFont.systemFont(ofSize: 16)
         
-        return textField
+        return textView
+    }()
+    
+    private let placeholderLabel: UILabel = {
+        let label = UILabel()
+        label.text = "내용을 입력해주세요"
+        label.textColor = .lightGray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
     }()
     
     var textChanged: PublishSubject<String> = PublishSubject()
@@ -22,7 +31,7 @@ final class DescriptionCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setupTitleCell()
-        descriptionTextField.addTarget(self, action: #selector(textViewDidChange(_:)), for: .editingChanged)
+        setupNotification()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -30,15 +39,67 @@ final class DescriptionCell: UITableViewCell {
     }
     
     private func setupTitleCell() {
-        contentView.addSubview(descriptionTextField)
+        contentView.addSubview(descriptionTextView)
+        descriptionTextView.addSubview(placeholderLabel)
         
+        descriptionTextView.textContainerInset = UIEdgeInsets(top: 2, left: 0, bottom: 4, right: 0)
+        descriptionTextView.textContainer.lineFragmentPadding = 0
         NSLayoutConstraint.activate([
-            descriptionTextField.topAnchor.constraint(equalTo: topAnchor, constant: 16),
-            descriptionTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24)
+            descriptionTextView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16),
+            descriptionTextView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 24),
+            descriptionTextView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -24),
+            descriptionTextView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16),
+            
+            placeholderLabel.topAnchor.constraint(equalTo: descriptionTextView.topAnchor),
+            placeholderLabel.leadingAnchor.constraint(equalTo: descriptionTextView.leadingAnchor),
+            placeholderLabel.trailingAnchor.constraint(equalTo: descriptionTextView.trailingAnchor, constant: -4)
         ])
+        
+        updatePlaceholderVisibility()
     }
     
-    @objc func textViewDidChange(_ textField: UITextField) {
-        textChanged.onNext(textField.text ?? "")
+    func textViewDidChange(_ textView: UITextView) {
+        textChanged.onNext(textView.text)
+        updatePlaceholderVisibility()
+    }
+    
+    private func updatePlaceholderVisibility() {
+        placeholderLabel.isHidden = !descriptionTextView.text.isEmpty
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+}
+
+//MARK: KeyboardAction
+extension DescriptionCell {
+    
+    private func setupNotification() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil
+        )
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil
+        )
+    }
+    
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        guard let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey]
+                as? CGRect else { return }
+        
+        let keyboardHeight = keyboardFrame.size.height
+        descriptionTextView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardFrame.size.height, right: 0)
+        descriptionTextView.scrollIndicatorInsets = descriptionTextView.contentInset
+    }
+    
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        descriptionTextView.contentInset = .zero
+        descriptionTextView.scrollIndicatorInsets = .zero
     }
 }
