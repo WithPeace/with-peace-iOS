d69acd24ea130ac012a85facf36d69e74fc2ee36
Hemg <101572902+hemg2@users.noreply.github.com>
2024-03-21 15:16:44 +0900

    Feat/#24 post view구현 (#31)
    * feat: postView NaviBar custom
    
    * feat: PostNavigationBarView 파일분리
    
    * feat: PostView 구현
    
    * feat: 카테고리 뷰 구현
    
    * feat: 카테고리 선택,  버튼 활성화
    
    * refactor: rx적용 진행 VC->VM
    
    * feat: 글등록시 PostMdeol 데이터저장
    
    * refactor: PostModel변경
    
    * refactor: image URL -> Data 변경
    
    * refactor: Description text키보드액션 추가
    
    * refactor: TitleCell text키보드액션 추가
    
    * refactor: cell 수정
    
    ---------
    
    Co-authored-by: Dylan-Yoon <77507952+Dylan-yoon@users.noreply.github.com>

---
+++ WithPeace/WithPeace/View/Post/Cell/CategoryViewCell.swift
@@ -0,0 +1,60 @@
+//
+//  CategoryViewCell.swift
+//  WithPeace
+//
+//  Created by Hemg on 3/19/24.
+//
+
+import UIKit
+
+final class CategoryViewCell: UITableViewCell {
+    private let categoryLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    private let selectButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(named: Const.CustomIcon.ICBtnPostcreate.icSelect), for: .normal)
+        
+        return button
+    }()
+    
+    var onButtonTapped: (() -> Void)?
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupCategoryCell()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    private func setupCategoryCell() {
+        contentView.addSubview(categoryLabel)
+        contentView.addSubview(selectButton)
+        selectionStyle = .none
+        
+        NSLayoutConstraint.activate([
+            categoryLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
+            categoryLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            
+            selectButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -24),
+            selectButton.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+        
+        selectButton.addTarget(self, action: #selector(didTapSelectButton), for: .touchUpInside)
+    }
+    
+    func configure(_ category: String) {
+        categoryLabel.text = category
+    }
+    
+    @objc private func didTapSelectButton() {
+        onButtonTapped?()
+    }
+}
