fd78c61f5266b90f876b59edfb8a84fb48a1668d
Dylan-Yoon <77507952+Dylan-yoon@users.noreply.github.com>
2024-03-25 00:08:59 +0900

    Feat/#26 회원가입refactoring (#41)
    * refactor: View 기획에 맞게 구현
    
    * feat: imageView Viewmodel통해 변경 구현
    - GestureRecognizer
    - imageView image
    
    * feat: 중복 확인 전 구현 완료

--- WithPeace/WithPeace/View/Login/NickNameView/LoginNickNameViewController.swift
+++ WithPeace/WithPeace/View/Login/NickNameView/LoginNickNameViewController.swift
@@ -7,21 +7,35 @@
 
 import UIKit
 import RxSwift
+import Photos
 
 class LoginNickNameViewController: UIViewController {
     
     private let viewModel = LoginNickNameViewModel()
     private let disposeBag = DisposeBag()
     private let bottomLayer = CALayer()
+    private var toastMessage: ToastMessageView?
     
     private let titleLabel: UILabel = {
         let label = UILabel()
         
-        label.text = "사용할 닉네임을 정해주세요!"
+        label.text = "프로필을 설정해주세요!"
         label.font = .preferredFont(forTextStyle: .title3)
         
         return label
     }()  
+    
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        #warning("Profile default Image Change")
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 60
+        imageView.image = UIImage(named: Const.Logo.MainLogo.withpeaceLogo)
+        
+        return imageView
+    }()
+    
     
     private let bodyLabel: UILabel = {
         let label = UILabel()
@@ -42,22 +56,33 @@ class LoginNickNameViewController: UIViewController {
         return textField
     }()
     
-    private let logoImageView: UIImageView = {
-        let imageView = UIImageView()
+    private let errorLabel: UILabel = {
+        let label = UILabel()
         
-        imageView.image = UIImage(named: Const.Logo.MainLogo.withpeaceLogo)
+        label.textColor = .red
+        label.font = .preferredFont(forTextStyle: .caption1)
         
-        return imageView
+        return label
     }()
     
     private let registButton: UIButton = {
         let button = UIButton()
-        
+       
+        button.backgroundColor = UIColor(named: Const.CustomColor.BrandColor2.mainPurple)
         button.setTitle("가입 완료", for: .normal)
         button.layer.cornerRadius = 9
         
         return button
     }()
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        self.toastMessage = ToastMessageView(superView: self.view)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -73,22 +98,44 @@ class LoginNickNameViewController: UIViewController {
         super.viewDidLayoutSubviews()
         
         bottomLayer.removeFromSuperlayer()
-        bottomLayer.backgroundColor = UIColor.black.cgColor
-        bottomLayer.frame = CGRect(x: 0, y: nicknameTextField.frame.size.height - 1, width: nicknameTextField.frame.width, height: 1)
+        
+        bottomLayer.frame = CGRect(x: 0, y: nicknameTextField.frame.size.height + 4, width: nicknameTextField.frame.width, height: 1)
         nicknameTextField.layer.addSublayer(bottomLayer)
+    }
+    
+    deinit {
+        debugPrint("DEINIT - LoginNickNameViewController")
     }
     
     private func bind() {
         viewModel.isNicknameValid
-            .subscribe {
-                if $0 {
-                    self.registButton.backgroundColor = UIColor(named: Const.CustomColor.BrandColor2.mainPurple)
-                } else {
-                    self.registButton.backgroundColor = UIColor(named: Const.CustomColor.SystemColor.gray2)
-                }
-                self.registButton.isEnabled = $0
+            .subscribe(onNext: { [weak self] emit in
+                self?.errorLabel.text = emit.description
+                self?.configureUI(isError: emit.isError)
+            })
+            .disposed(by: disposeBag)
+        
+        viewModel.changeProfileImage
+            .subscribe { [weak self] data in
+                guard let data = data else { return }
+                
+                self?.profileImageView.image = UIImage(data: data)
             }
             .disposed(by: disposeBag)
+        
+        viewModel.dismissForNext
+            .subscribe { [weak self] in
+                if $0.isError {
+                    self?.toastMessage?.presentStandardToastMessage("닉네임 등록을 완료해주세요")
+                } else {
+                    self?.dismiss(animated: true)
+                }
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    private func configureUI(isError: Bool) {
+        self.bottomLayer.backgroundColor = isError ? UIColor.red.cgColor : UIColor.black.cgColor
     }
 }
 
@@ -102,12 +149,33 @@ extension LoginNickNameViewController {
     
     @objc
     private func tapRegistButton() {
-        //TODO: 완료버튼 로직수행 -> mainView
+        viewModel.tapRegisterButton.onNext(())
     }
     
     private func configureTargetAction() {
         registButton.addTarget(self, action: #selector(tapRegistButton), for: .touchUpInside)
         nicknameTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+        
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(tapImage))
+        profileImageView.addGestureRecognizer(gesture)
+        profileImageView.isUserInteractionEnabled = true
+    }
+    
+    @objc
+    private func tapImage() {
+        let customPhotoAlbumViewController = CustomPhotoAlbumViewController(maxSelect: 1)
+        customPhotoAlbumViewController.modalPresentationStyle = .fullScreen
+        
+        self.present(customPhotoAlbumViewController, animated: true)
+        
+        //TODO: 이미지 변환할 때 까지 Button 비활성화 (비동기에 의한 데이터 변환 딜레이 대비)
+        customPhotoAlbumViewController.completionHandlerChangePHAssetsToDatas = { [weak self] in
+            guard let asset = $0.first else { return }
+            
+            PHImageManager().requestImageDataAndOrientation(for: asset, options: nil) { data, _, _, _ in
+                self?.viewModel.profileImage.onNext(data)
+            }
+        }
     }
 }
 
@@ -116,7 +184,7 @@ extension LoginNickNameViewController {
     
     private func configureLayout() {
         
-        [titleLabel, bodyLabel, nicknameTextField, logoImageView, registButton].forEach {
+        [titleLabel, profileImageView, bodyLabel, nicknameTextField, errorLabel, registButton].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
             view.addSubview($0)
         }
@@ -125,20 +193,23 @@ extension LoginNickNameViewController {
         
         NSLayoutConstraint.activate([
             titleLabel.centerXAnchor.constraint(equalTo: safe.centerXAnchor),
+            profileImageView.centerXAnchor.constraint(equalTo: safe.centerXAnchor),
             bodyLabel.centerXAnchor.constraint(equalTo: safe.centerXAnchor),
             nicknameTextField.centerXAnchor.constraint(equalTo: safe.centerXAnchor),
-            logoImageView.centerXAnchor.constraint(equalTo: safe.centerXAnchor),
+            errorLabel.centerXAnchor.constraint(equalTo: safe.centerXAnchor),
             registButton.centerXAnchor.constraint(equalTo: safe.centerXAnchor),
             
             titleLabel.topAnchor.constraint(equalTo: safe.topAnchor, constant: 121),
             
-            bodyLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
+            profileImageView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 24),
+            profileImageView.heightAnchor.constraint(equalToConstant: 120),
+            profileImageView.widthAnchor.constraint(equalToConstant: 120),
             
-            nicknameTextField.topAnchor.constraint(equalTo: bodyLabel.bottomAnchor, constant: 20),
-                                                   
-            logoImageView.topAnchor.constraint(equalTo: nicknameTextField.bottomAnchor, constant: 77),
-            logoImageView.heightAnchor.constraint(equalToConstant: 150),
-            logoImageView.widthAnchor.constraint(equalToConstant: 150),
+            bodyLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 24),
+            
+            nicknameTextField.topAnchor.constraint(equalTo: bodyLabel.bottomAnchor, constant: 16),
+            
+            errorLabel.topAnchor.constraint(equalTo: nicknameTextField.bottomAnchor, constant: 4),
             
             registButton.heightAnchor.constraint(equalToConstant: 55),
             registButton.leadingAnchor.constraint(equalTo: safe.leadingAnchor, constant: 24),
--- WithPeace/WithPeace/View/Login/NickNameView/LoginNickNameViewModel.swift
+++ WithPeace/WithPeace/View/Login/NickNameView/LoginNickNameViewModel.swift
@@ -9,23 +9,95 @@ import Foundation
 import RxSwift
 
 final class LoginNickNameViewModel {
+    enum State {
+        case success
+        case notEnoughCharacters
+        case exceededCharacters
+        case duplication
+        case otherCharacters
+        case unknown
+        
+        var description: String {
+            switch self {
+            case .success:
+                ""
+            case .notEnoughCharacters:
+                "닉네임은 2~10자의 한글, 영문만 가능합니다."
+            case .exceededCharacters:
+                "닉네임은 2~10자의 한글, 영문만 가능합니다."
+            case .duplication:
+                "중복된 닉네임 입니다"
+            case .otherCharacters:
+                "닉네임은 2~10자의 한글, 영문만 가능합니다."
+            case .unknown:
+                ""
+            }
+        }
+        
+        var isError: Bool {
+            switch self {
+            case .success:
+                false
+            case .notEnoughCharacters:
+                true
+            case .exceededCharacters:
+                true
+            case .duplication:
+                true
+            case .otherCharacters:
+                true
+            case .unknown:
+                false
+            }
+        }
+    }
     
+    let disposeBag = DisposeBag()
+    
+    //INPUT
     var nicknameField: BehaviorSubject<String?>
-    var isNicknameValid: Observable<Bool>
+    var profileImage: PublishSubject<Data?>
+    var tapRegisterButton: BehaviorSubject<Void>
+    
+    //OUTPUT
+    var isNicknameValid: Observable<State> // 닉네임이 유효한지?
+    
+    var changeProfileImage: Observable<Data?>
+    var dismissForNext: Observable<State>
     
     init() {
-        let nicknameField = BehaviorSubject<String?>(value: "")
         
-        self.nicknameField = nicknameField
+        //INPUT
+        self.nicknameField = BehaviorSubject<String?>(value: "")
+        self.profileImage = PublishSubject<Data?>()
+        self.tapRegisterButton = BehaviorSubject<Void>(value: Void())
         
-        self.isNicknameValid = nicknameField
+        //OUTPUT
+        self.isNicknameValid = self.nicknameField
             .map { nickname in
-                guard let nickname = nickname else { return false }
-                if nickname.count < 2 || nickname.count > 10 {
-                    return false
-                } else {
-                    return true
+                guard let nickname = nickname else { return .unknown }
+                
+                if nickname.count < 2  {
+                    return .notEnoughCharacters
+                } else if nickname.count > 10 {
+                    return .exceededCharacters
+                } else if !nickname.koreaLangCheck() {
+                    return .otherCharacters
                 }
+                
+                //TODO: 서버 닉네임 중복 확인. 임시구현
+                if nickname == "abc" { return .duplication }
+                
+                return .success
             }
+        
+        self.changeProfileImage = profileImage
+            .map { data in
+                if let data = data { return data }
+                
+                return nil
+            }
+        
+        dismissForNext = tapRegisterButton.withLatestFrom(isNicknameValid)
     }
 }
