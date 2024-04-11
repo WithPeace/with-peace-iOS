5e3a38b91cd41381195ecf5bd343e944f87b3ad0
Dylan-Yoon <77507952+Dylan-yoon@users.noreply.github.com>
2024-04-11 19:31:58 +0900

    Feat/#63 sign repository api변경에 따른 refactoring (#67)
    * feat: 코드 순서 조정, reauthorization -> authorization 변경
    
    * feat: multipart form data 수정 및 Authentication 관련 API 수정

--- WithPeace/WithPeace/Manager/API/SignRepository.swift
+++ WithPeace/WithPeace/Manager/API/SignRepository.swift
@@ -10,9 +10,10 @@ import Foundation
 protocol AuthenticationProvider {
     func performGoogleSign(idToken: String,
                            completion: @escaping (Result<SignAuthDTO, SignRepositoryError>) -> Void)
-    func performRefresh(completion: @escaping (Result<Void, SignRepositoryError>) -> Void)
     func performRegister(nickname: String,
+                         imageData: Data?,
                          completion: @escaping (Result<Void, SignRepositoryError>) -> Void)
+    func performRefresh(completion: @escaping (Result<Void, SignRepositoryError>) -> Void)
     func performLogout(completion: @escaping (Result<Void, SignRepositoryError>) -> Void)
 }
 
@@ -57,14 +58,101 @@ final class SignRepository: AuthenticationProvider {
         }
     }
     
+    //TODO: 회원가입 메서드 (회원가입 완료 시 : role: guest -> user)
+    func performRegister(nickname: String,
+                         imageData: Data? = nil,
+                         completion: @escaping (Result<Void, SignRepositoryError>) -> Void) {
+        guard let baseURL = Bundle.main.apiKey else {
+            completion(.failure(.bundleError))
+            return
+        }
+        
+        guard let keychainAccessToken = keychainManager.get(account: "accessToken"),
+              let keychainAccessToken = String(data: keychainAccessToken, encoding: .utf8) else {
+            completion(.failure(.notKeychain))
+            return
+        }
+        
+        var formData = MultipartFormData()
+        formData.createFormFiled(contents: ["nickname":nickname])
+        
+        if let imageData = imageData {
+            formData.addFilesBodyData(fieldName: "imageFile",
+                                      fileName: "image.jpeg",
+                                      mimeType: "image/jpeg",
+                                      fileData: imageData)
+        }
+        
+        let requestBody = formData.generateData()
+        
+        var contentsTypeHeader = formData.generateHeader()
+        contentsTypeHeader["Authorization"] = "Bearer \(keychainAccessToken)"
+//        var contentsTypeHeader = ["Authorization":"Bearer \(keychainAccessToken)"]
+//        contentsTypeHeader["Content-Type"] = "multipart/form-data; boundary=\(formData.getBoundary())"
+        
+        
+        let endPoint = EndPoint(baseURL: baseURL,
+                                path: "/api/v1/auth/register",
+                                port: 8080,
+                                scheme: "http",
+                                headers: contentsTypeHeader,
+                                method: .post,
+                                body: requestBody)
+        
+        NetworkManager.shared.fetchData(endpoint: endPoint) { result in
+            switch result {
+            case .success(let data):
+                do {
+                    guard let signDTO = try? JSONDecoder().decode(SignAuthDTO.self, from: data) else {
+                        completion(.failure(.decodingError))
+                        return
+                    }
+                    
+                    guard let accessToken = signDTO.data.accessToken,
+                          let refreshToken = signDTO.data.refreshToken else {
+                        completion(.failure(.tokenAbsenceError))
+                        return
+                    }
+                    
+                    try self.saveTokens(accessToken: accessToken, refreshToken: refreshToken)
+                    completion(.success(()))
+                } catch {
+                    completion(.failure(.decodingError))
+                    return
+                }
+            case .failure(let error):
+                if error == .unauthorized {
+                    //TODO: 401 일 때 리프레쉬로 엑세스 먼저 발급 40101일 경우만 refresh실행
+                    self.performRefresh { result in
+                        switch result {
+                        case .success(_):
+                            break
+                        case .failure(_):
+                            //TODO: LogOut
+                            debugPrint("PERFORM REGISTER _ REFRESH ERROR : Please LogOut")
+                            
+                            break
+                        }
+                    }
+                } else {
+                    completion(.failure(.networkError))
+                }
+            }
+        }
+    }
+    
     func performRefresh(completion: @escaping (Result<Void, SignRepositoryError>) -> Void) {
         guard let baseURL = Bundle.main.apiKey else {
             completion(.failure(.bundleError))
             return
         }
         
-        guard let keychainAccessToken = keychainManager.get(account: "refreshToken") else {
+        guard let keychainRefreshToken = keychainManager.get(account: "refreshToken") else {
             completion(.failure(.notKeychain))
+            return
+        }
+        
+        guard let keychainRefreshToken = String(data: keychainRefreshToken, encoding: .utf8) else {
             return
         }
         
@@ -72,7 +160,7 @@ final class SignRepository: AuthenticationProvider {
                                 path: "/api/v1/auth/refresh",
                                 port: 8080,
                                 scheme: "http",
-                                headers: ["Reauthorization":"Bearer \(keychainAccessToken)"],
+                                headers: ["Authorization":"Bearer \(keychainRefreshToken)"],
                                 method: .post)
         
         NetworkManager.shared.fetchData(endpoint: endPoint) { result in
@@ -100,76 +188,6 @@ final class SignRepository: AuthenticationProvider {
                     // 서버에서 우리가 로그아웃 하지 않았는데, 로그인이 되었다면 어떻게 되는것인가?
                     //      만약 서버에서 로그인시도가 있을 때 자동 로그아웃 처리되면 우리는 그냥 로그인다시하면됨.
                     // 하지만 서버에서 로그아웃 -> 로그인 을 원하면? 그건 다시생각야함.
-                } else {
-                    completion(.failure(.networkError))
-                }
-            }
-        }
-    }
-    
-    //TODO: 회원가입 메서드 (회원가입 완료 시 : role: guest -> user)
-    func performRegister(nickname: String,
-                         completion: @escaping (Result<Void, SignRepositoryError>) -> Void) {
-        guard let baseURL = Bundle.main.apiKey else {
-            completion(.failure(.bundleError))
-            return
-        }
-        
-        guard let keychainAccessToken = keychainManager.get(account: "accessToken") else {
-            completion(.failure(.notKeychain))
-            return
-        }
-        
-        let nicknameModel = Nickname(nickname: nickname)
-        
-        guard let requestBody = try? JSONEncoder().encode(nicknameModel) else {
-            completion(.failure(.encodingError))
-            return
-        }
-        
-        let endPoint = EndPoint(baseURL: baseURL,
-                                path: "/api/v1/auth/register",
-                                port: 8080,
-                                scheme: "http",
-                                headers: ["Authorization":"Bearer \(keychainAccessToken)"],
-                                method: .post,
-                                body: requestBody)
-        
-        NetworkManager.shared.fetchData(endpoint: endPoint) { result in
-            switch result {
-            case .success(let data):
-                do {
-                    guard let signDTO = try? JSONDecoder().decode(SignAuthDTO.self, from: data) else {
-                        completion(.failure(.decodingError))
-                        return
-                    }
-                    
-                    guard let accessToken = signDTO.data.accessToken,
-                          let refreshToken = signDTO.data.refreshToken else {
-                        completion(.failure(.tokenAbsenceError))
-                        return
-                    }
-                    
-                    try self.saveTokens(accessToken: accessToken, refreshToken: refreshToken)
-                    completion(.success(()))
-                } catch {
-                    completion(.failure(.decodingError))
-                    return
-                }
-            case .failure(let error):
-                if error == .unauthorized {
-                    //401 일 때 리프레쉬로 엑세스 먼저 발급
-                    self.performRefresh { result in
-                        switch result {
-                        case .success(_):
-                            self.performRegister(nickname: nickname, completion: completion)
-                        case .failure(_):
-                            //TODO: LogOut
-                            debugPrint("PERFORM REGISTER _ REFRESH ERROR : Please LogOut")
-                            
-                            break
-                        }
-                    }
                 } else {
                     completion(.failure(.networkError))
                 }
@@ -215,7 +233,10 @@ final class SignRepository: AuthenticationProvider {
             }
         }
     }
-    
+}
+
+//MARK: - Private Function
+extension SignRepository {
     private func saveTokens(accessToken: String, refreshToken: String) throws {
         guard let refreshToken = refreshToken.data(using: .utf8),
               let accessToken = accessToken.data(using: .utf8) else {
