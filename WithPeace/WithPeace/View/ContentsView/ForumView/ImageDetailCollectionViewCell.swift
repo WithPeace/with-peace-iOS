bc5abf214782579d5599afc5523e77a6894c99d6
Hemg <101572902+hemg2@users.noreply.github.com>
2024-04-16 19:23:03 +0900

    Feat/#64 post detail view (#66)
    * feat: PostList 생성
    
    * feat: categoryView UI 구현
    
    * feat: category에 맞게 view 생성
    
    * style: 코드지우기 (버튼 셀렉색x)
    
    * refactor: 카테고리 선택시 버튼 셀렉
    
    * style: categoryView 분리
    
    * refactor: assetImage size수정
    
    * refactor: 등록 후 돌아오는 tab 설정
    
    * feat: didSelectItemAt: 디테일 뷰생성
    
    * feat: DetailViewCell, ImageDetailCell 생성
    
    * refactor: UUID추가, 이미지 delete 추가
    
    * refactor: 수정 작업 진행(API로 해야해서 스탑)
    
    * refactor: 수정하기전
    
    * refactor: 액션시트, 분기처리 예시
    
    * feat: 시간순서 게시글 업로드
    
    ---------
    
    Co-authored-by: dylan-yoon <d.dylany21@gmail.com>
    Co-authored-by: Dylan-Yoon <77507952+Dylan-yoon@users.noreply.github.com>

---
+++ WithPeace/WithPeace/View/ContentsView/ForumView/ImageDetailCollectionViewCell.swift
@@ -0,0 +1,50 @@
+//
+//  ImageDetailCollectionViewCell.swift
+//  WithPeace
+//
+//  Created by Hemg on 4/11/24.
+//
+
+import UIKit
+
+final class ImageDetailCollectionViewCell: UICollectionViewCell {
+    private let imageView: UIImageView = {
+        let image = UIImageView()
+        image.contentMode = .scaleAspectFill
+        image.clipsToBounds = true
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.layer.cornerRadius = 10
+        
+        return image
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        imageView.image = nil
+    }
+    
+    private func setupViews() {
+        contentView.addSubview(imageView)
+        contentView.clipsToBounds = false
+        
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 4),
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 24),
+            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -24),
+        ])
+    }
+    
+    func configure(image: UIImage) {
+        imageView.image = image
+    }
+}
