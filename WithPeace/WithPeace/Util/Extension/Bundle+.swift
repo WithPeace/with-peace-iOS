2c6e9319195ddf2bc2a4c72214d98315d11def3c
Dylan-Yoon <77507952+Dylan-yoon@users.noreply.github.com>
2024-04-28 23:25:24 +0900

    feat: API 구현 (#78)

--- WithPeace/WithPeace/Util/Extension/Bundle+.swift
+++ WithPeace/WithPeace/Util/Extension/Bundle+.swift
@@ -9,11 +9,20 @@ import Foundation
 
 extension Bundle {
     var apiKey: String? {
-            guard let file = self.path(forResource: "Secrets", ofType: "plist"),
-                  let resource = NSDictionary(contentsOfFile: file),
-                  let key = resource["API_KEY"] as? String else {
-                return nil
-            }
-            return key
+        guard let file = self.path(forResource: "Secrets", ofType: "plist"),
+              let resource = NSDictionary(contentsOfFile: file),
+              let key = resource["API_KEY"] as? String else {
+            return nil
         }
+        return key
+    }
+    
+    var youthcenterAPIkey: String? {
+        guard let file = self.path(forResource: "Secrets", ofType: "plist"),
+              let resource = NSDictionary(contentsOfFile:  file),
+              let key = resource["YouthCenter_API_KEY"] as? String else {
+            return nil
+        }
+        return key
+    }
 }
